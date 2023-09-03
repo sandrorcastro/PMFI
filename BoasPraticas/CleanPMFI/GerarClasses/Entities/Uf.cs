f03e6bd556ebea9157b9c6a0c2b338d0b5035319
Sandro Castro <sandrorcastro@hotmail.com>
2023-09-03 18:54:35 -0300

    salvando

---
+++ BoasPraticas/CleanPMFI/GerarClasses/Entities/Uf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GerarClasses.Entities
+{
+    public partial class Uf
+    {
+        public Uf()
+        {
+            Cidades = new HashSet<Cidade>();
+        }
+
+        public int IdUf { get; set; }
+        public string? Sigla { get; set; }
+        public string Nome { get; set; } = null!;
+        public int IdPais { get; set; }
+        public int? CodigoIbge { get; set; }
+
+        public virtual Pais IdPaisNavigation { get; set; } = null!;
+        public virtual ICollection<Cidade> Cidades { get; set; }
+    }
+}
