e76fcb577c66c154059aefedbbb93e7f3122ddf1
Sandro Castro <sandrorcastro@hotmail.com>
2023-11-27 20:57:35 -0300

    bk

--- BoasPraticas/FiscalMobile/FiscalMobile/Platforms/Android/MainApplication.cs
+++ BoasPraticas/FiscalMobile/FiscalMobile/Platforms/Android/MainApplication.cs
@@ -8,7 +8,7 @@ using Android.Runtime;
 
 // Needed for Taking photo/video
 [assembly: UsesPermission(Android.Manifest.Permission.Camera)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage,  MaxSdkVersion = 32)]
 
 // Add these properties if you would like to filter out devices that do not have cameras, or set to false to make them optional
 [assembly: UsesFeature("android.hardware.camera", Required = true)]
